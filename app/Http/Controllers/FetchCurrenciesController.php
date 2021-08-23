8b1e53cccb7aefae324023d9591bd8af35dc7c04
tech <benzema-benarfa@bk.ru>
2021-08-24 00:00:08 +0500

    first commit

---
+++ app/Http/Controllers/FetchCurrenciesController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Currency;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class FetchCurrenciesController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $response = Http::get('https://www.cbr-xml-daily.ru/daily_json.js');
+        $currencies = $response->collect('Valute');
+        $currencies->each(function ($currency) {
+            $this->saveCurrency($currency);
+        });
+
+        return response('', Response::HTTP_OK);
+
+    }
+
+    private function saveCurrency($data)
+    {
+        Currency::query()->updateOrCreate(
+            ['char_code' => $data['CharCode']],
+            [
+                'name' => $data['Name'],
+                'rate' => $data['Value'] / $data['Nominal'],
+                'updated_at' => now(),
+            ]
+        );
+    }
+}
---
+++ database/migrations/2021_08_23_181833_create_currencies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id();
+            $table->string('char_code',3)->unique()->comment('Currency code');
+            $table->string('name',100)->comment('Currency name');
+            $table->float('rate')->comment('Ru relation');
+//            $table->text('desc')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
